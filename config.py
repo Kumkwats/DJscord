0d0d5e65b5775a44a5e1604d58116f193c6dd485
Kumkwats <endercraft431@gmail.com>
2022-01-04 10:34:58 +0100

    DL and Sound folder configurable from config file

--- config.py
+++ config.py
@@ -14,17 +14,39 @@ class Config():
             self.conf = json.load(f)
             f.close()
 
+            print("Config: Will write and read files at those directories (relative to working dir):")
+            
+            self.downloadDirectory = "downloads/" # default downloads directory
+            if 'download-director' in self.conf:
+                self.downloadDirectory = self.conf['download-directory']
+            
+            print("Downloaded files at: " + self.downloadDirectory)
+
+            self.soundDirectory = "sounds/" # default sounds directory
+            if 'sound-directory' in self.conf:
+                self.soundDirectory = self.conf['sound-directory']
+            
+            print("Other sound files at: " + self.soundDirectory)
+
             self.spotifyEnabled = False
             if 'spotify-client-id' and 'spotify-client-secret' in self.conf:
                 self.spotifyEnabled = True
-
+                print("Config: Spotify research is enabled")
+            else:
+                print("Config: Spotify research is disabled")
+            
+            
             self.FoxDotEnabled = False
             if "FoxDot-port" and "FoxDot-address" in self.conf:
                 self.FoxDotEnabled = True
+                print("Config: FoxDot is enabled")
+            else:
+                print("Config: FoxDot is disabled")
+
 
             self.token = self.conf['discord-token']
         else:
-            print("Fichier config non trouvé ou non valide:", CFGFILE)
+            print("ERR: Config file is missing (%s)" % CFGFILE)
             exit()
 
     @classmethod
