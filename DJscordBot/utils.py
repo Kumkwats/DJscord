4b310cc941e7ff37575036d68e9f8caaf73c867f
Kumkwats <endercraft431@gmail.com>
2025-10-05 10:22:21 +0200

    fix time format for time greater than 24 hours

--- DJscordBot/utils.py
+++ DJscordBot/utils.py
@@ -5,26 +5,33 @@ import os
 import random
 from DJscordBot.config import config
 
-def time_format(seconds: int) -> str:
+def time_format(time_in_seconds: int) -> str:
     """Format the time in a more readable manner
 
     Args:
-        seconds (int): 
+        time_in_seconds (int): the time to format in seconds
 
     Returns:
-        str: formatted time (h:mm:ss) or (m:ss)
+        str: Formatted time.
+        
+        Formats: (d:hh:mm:ss) or (h:mm:ss) or (m:ss) depending of the highest factor
     """
 
 
-    seconds = int(seconds)
-    h = seconds // 3600 % 24
-    m = seconds % 3600 // 60
-    s = seconds % 60
-
-    if h > 0:
-        return f"{h}:{m:0>2}:{s:0>2}"
-    else:
-        return f"{m}:{s:0>2}"
+    time_in_seconds = int(time_in_seconds)
+    
+    days = time_in_seconds // 86400
+    hrs = time_in_seconds // 3600 % 24
+    min = time_in_seconds % 3600 // 60
+    sec = time_in_seconds % 60
+    
+    if days > 0:
+        return f"{days}:{hrs:0>2}:{min:0>2}:{sec:0>2}"
+    
+    if hrs > 0:
+        return f"{hrs}:{min:0>2}:{sec:0>2}"
+    
+    return f"{min}:{sec:0>2}"
 
 
 def pick_sound_file(folder_name: str) -> tuple[bool, str]:
