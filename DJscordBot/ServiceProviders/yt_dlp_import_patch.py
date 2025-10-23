1679ad9474bde60ba58e57b441d99ac6e6c6e7a0
Kumkwats <endercraft431@gmail.com>
2025-10-23 22:52:25 +0200

    update yt-dlp + new patch

--- DJscordBot/ServiceProviders/yt_dlp_import_patch.py
+++ DJscordBot/ServiceProviders/yt_dlp_import_patch.py
@@ -6,10 +6,16 @@ import importlib
 #     BUG_REPORT_MESSAGE = bug_reports_message(before='')
 #                          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # TypeError: <lambda>() got an unexpected keyword argument 'before'
-YT_DLP_PROVIDER_UNPATCHED = 'yt_dlp.extractor.youtube.pot._provider'
-YT_DLP_PROVIDER_PATCHED = 'DJscordBot.ServiceProviders.yt_dlp_patches.yt_dlp__provider'
 
-if importlib.util.find_spec(YT_DLP_PROVIDER_PATCHED) is not None:
-    __import__(YT_DLP_PROVIDER_PATCHED)
-    sys.modules[YT_DLP_PROVIDER_UNPATCHED] = sys.modules.pop(YT_DLP_PROVIDER_PATCHED)
-    print('Patched yt-dlp provider')
+def patch(source, patch, name):
+    if importlib.util.find_spec(patch) is not None:
+        __import__(patch)
+        sys.modules[source] = sys.modules.pop(patch)
+        print(f'Patched yt-dlp {name}')
+
+patch('yt_dlp.extractor.youtube.pot._provider',
+      'DJscordBot.ServiceProviders.yt_dlp_patches.yt_dlp__provider',
+      "pot._provider")
+patch('yt_dlp.extractor.youtube._provider',
+      'DJscordBot.ServiceProviders.yt_dlp_patches.yt_dlp__video',
+      "_video")
